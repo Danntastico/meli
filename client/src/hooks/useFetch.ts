9887b6b7c8b18fcd9546076d980f270eb5769d92
Dann <danilo.pena@ascential.com>
2023-05-26 01:29:13 -0500

    🏗 useFetch Hook

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { FetchFn, State } from "../types/helpers";
+
+export const useFetch = <T>(fetchFunction: FetchFn, ...params: unknown[]): State<T> => {
+
+  const [state, setState] = useState<State<T>>({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    fetchFunction(...params)
+      .then((res) => {
+        setState({
+          loading: false,
+          error: null,
+          data: res as T,
+        });
+      })
+      .catch((error) => {
+        setState({
+          data: null,
+          loading: false,
+          error,
+        });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetchFunction]);
+
+  return state;
+};
